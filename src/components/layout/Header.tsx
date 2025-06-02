5f407c31cf2588c9df79cae68ed0ef112f66fa16
ahmad seyam <ahmad.seyam.maw@gmail.com>
2025-06-02 17:03:47 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the text content:
    
        صُنّاع الأثَرالرئيسيةالمعسكرات الصيفية/ المسائيةالبرامج التدريبيةالمتجراستضافة للمؤسساتانضم كمدربسجّل الآندخول
    
    Relevant files:
    
        - https://6000-firebase-studio-1748866068769.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    قم بتحريك "استضافة للمؤسسات" إلى يمين المساحة المخصصة لل "المتجر"، ثم قم يبتغيير النص ليكون "طلبات استضافة. لبرامجان"

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,8 +25,8 @@ const dropdownNavLinks = [
 ];
 
 const secondaryNavLinks = [
+  { href: '/institutions/hosting-request', label: 'طلبات استضافة لبرامجنا', icon: <Briefcase className="me-1 h-4 w-4" /> },
   { href: '/store', label: 'المتجر', icon: <Store className="me-1 h-4 w-4" /> },
-  { href: '/institutions/hosting-request', label: 'استضافة للمؤسسات', icon: <Briefcase className="me-1 h-4 w-4" /> },
   { href: '/trainers/apply', label: 'انضم كمدرب', icon: <UserCheck className="me-1 h-4 w-4" /> },
 ];
 
